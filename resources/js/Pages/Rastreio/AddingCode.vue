53bd10e19f22170fcd067059dc77b577e68e588d
Vinnicyus Carvalho <vynny.cg@gmail.com>
2022-02-28 16:45:56 -0300

    Inicio do projeto

---
+++ resources/js/Pages/Rastreio/AddingCode.vue
@@ -0,0 +1,23 @@
+<template>
+    <jet-input></jet-input>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    import { defineComponent } from 'vue'
+    import JetInput  from '@/Jetstream/Input'
+    import JetInputError from '@/Jetstream/InputError'
+
+    export default defineComponent({
+        components: {
+            JetInput,
+            JetInputError
+        },
+        methods: {
+
+        }
+    })
+</script>
